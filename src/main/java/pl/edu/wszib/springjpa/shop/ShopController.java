25642fb7c7a5a537ede9454a19ee4aae6239a6b0
Ewa Klimkowska <eklimkow@student.wszib.edu.pl>
2023-04-17 21:26:15 +0200

    shop project

--- src/main/java/pl/edu/wszib/springjpa/shop/ShopController.java
+++ src/main/java/pl/edu/wszib/springjpa/shop/ShopController.java
@@ -1,4 +1,25 @@
 package pl.edu.wszib.springjpa.shop;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/shop")
 public class ShopController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping("/create")
+    public String create (Model model){
+        Product newProduct = new Product();
+        Product.Category[] categories = Product.Category.values();
+        model.addAttribute("newProduct", newProduct);
+        model.addAttribute("categories", categories);
+        return "shop/create";
+    }
+
 }
